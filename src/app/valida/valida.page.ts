c4fb1f68c7e07b682b72477748b90d61b14f32b1
poluceno <luizevp@hotmail.com>
2020-10-18 20:33:23 -0300

    valida1
    Valida

--- src/app/valida/valida.page.ts
+++ src/app/valida/valida.page.ts
@@ -8,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class ValidaPage implements OnInit {
 
   constructor() { }
+  alerta1(){
+    alert("Por favor, contate um administrador");
+    }
+validaEntrada(){
+  let validaEntrada = document.getElementById("nomeValida").value;
+  let senhaEntrada = document.getElementById("senhaValida").value;
+  if(validaEntrada == "")
+  {
+    document.getElementById("p1").innerHTML = "Digite um usuário";
+    document.getElementById("nomeValida").focus();
+    validaEntrada();
+    return false;
+  }
+  if(senhaEntrada == ""){
+    document.getElementById("p1").innerHTML = "Digite uma senha";
+    validaEntrada();
+    return false;
+  }
+  if(validaEntrada == "admin" && senhaEntrada == "admin"){
+    window.location.href = "carrinho";
+    return false;
 
+  }
+  if(validaEntrada != "admin" && senhaEntrada != "admin"){
+    alert("Usuário ou senha inválidos");
+    validaEntrada();
+    return false;
+  }
+else{
+  alert("Contate um administrador")
+}
+}
   ngOnInit() {
   }
 
