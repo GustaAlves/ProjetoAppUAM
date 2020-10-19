909c401586f3ff478d6e041c3791e1914403e004
poluceno <luizevp@hotmail.com>
2020-10-18 22:17:21 -0300

    tirando o lixo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,35 +31,4 @@ export class AppComponent {
     });
     
   }
-  alerta1(){
-    alert("Por favor, contate um administrador");
-    }
-validaEntrada(){
-  let validaEntrada = document.getElementById("nomeValida").text;
-  let senhaEntrada = document.getElementById("senhaValida").text;
-  if(validaEntrada == "")
-  {
-    document.getElementById("p1").innerHTML = "Digite um usuário";
-    document.getElementById("nomeValida").focus();
-    validaEntrada();
-    return false;
   }
-  if(senhaEntrada == ""){
-    document.getElementById("p1").innerHTML = "Digite uma senha";
-    validaEntrada();
-    return false;
-  }
-  if(validaEntrada == "admin" && senhaEntrada == "admin"){
-    window.location.href = "home/carrinho";
-    return false;
-
-  }
-  if(validaEntrada != "admin" && senhaEntrada != "admin"){
-    alert("Usuário ou senha inválidos");
-    validaEntrada();
-    return false;
-  }
-else{
-  alert("Contate um administrador")
-}
-}}
