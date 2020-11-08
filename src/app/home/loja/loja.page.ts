6793ce314e3c217c49e0fd1b46b4ebca7f6c1526
poluceno <luizevp@hotmail.com>
2020-11-08 20:17:23 -0300

    teste dados

--- src/app/home/loja/loja.page.ts
+++ src/app/home/loja/loja.page.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Produto{
+  nome : string;
+  tamanho: string;
+  cor : string;
+  publico : string;
+  valor : string;
+  contato : string;
+}
+
 @Component({
   selector: 'app-loja',
   templateUrl: './loja.page.html',
   styleUrls: ['./loja.page.scss'],
 })
 export class LojaPage implements OnInit {
-
+ 
+  public produto : Produto[] = [
+    {nome: 'Camisa 1', tamanho:'M', cor:'Azul', publico:'Masculino', valor:'30', contato:'(11) 4002-8922'}
+  ]
+  
   constructor() { }
 
   ngOnInit() {
