608851a3f45846a3fa2ddc3febfab3f488c3c154
poluceno <luizevp@hotmail.com>
2020-12-04 21:48:36 -0300

    fixing code bugs

--- src/app/home/loja/excluir/excluir.page.ts
+++ src/app/home/loja/excluir/excluir.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ProductService, Produto } from 'src/app/services/product.service';
-import { ProductDetailsComponent } from 'src/app/home/product-details/product-details.component';
 
 @Component({
   selector: 'app-excluir',
@@ -9,22 +6,8 @@ import { ProductDetailsComponent } from 'src/app/home/product-details/product-de
   styleUrls: ['./excluir.page.scss'],
 })
 export class ExcluirPage implements OnInit {
-  public produto = this.productService.all();
-  
-  constructor(
-    private modalControler: ModalController,
-    private productService: ProductService
-    ) { }
-  
-  public async openModal(produto : Produto){
-    const modal = await this.modalControler.create({
-      component: ProductDetailsComponent,
-      componentProps:{
-        produto
-      }
-    });
-    modal.present();
-  }
+
+  constructor() { }
 
   ngOnInit() {
   }
