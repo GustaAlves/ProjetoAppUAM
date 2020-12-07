0ce4801875343eea2f7fb7611fb363e1cf935151
poluceno <luizevp@hotmail.com>
2020-12-06 22:06:22 -0300

    out boot strap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { IonicStorageModule } from '@ionic/storage';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule {}
